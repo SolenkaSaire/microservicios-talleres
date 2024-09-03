49009e5e0e650507b8ef77627b95227268dc88c0
SolenkaSaire <solenkam.sairec@uqvirtual.edu.co>
2024-09-03 08:50:36 -0500

    [SMSC] parte 2.2

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/userRoutes');
 const authRoutes = require('./routes/authRoutes');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const path = require('path');
 require('dotenv').config();
 require('./config/dbConfig');
 
@@ -9,7 +12,36 @@ const app = express();
 
 app.use(bodyParser.json());
 
+// Cargar el archivo de especificación OpenAPI
+const swaggerDocument = YAML.load(path.join(__dirname, './openapi.yaml'));
+
+// Ruta para la documentación de Swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// Definir una ruta para la raíz
+app.get('/', (req, res) => {
+  res.send('Bienvenido a la API del taller!');
+});
+
 // Rutas
+/**
+ * @swagger
+ * /api/users:
+ *  get:
+ *   summary: Obtiene todos los usuarios
+ *   description: Obtiene todos los usuarios
+ *   responses:
+ *      200:
+ *          description: OK
+ *         content:
+ *          application/json:
+ *             schema:
+ *                type: array
+ *  
+ *      404:
+ *          description: No se encontraron usuarios
+ *
+ */
 app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
 
--- src/server.js
+++ src/server.js
@@ -9,17 +9,6 @@ const path = require('path');
 // Ruta para la documentación de Swagger
 //app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-// Cargar el archivo de especificación OpenAPI
-const swaggerDocument = YAML.load(path.join(__dirname, './openapi.yaml'));
-
-// Ruta para la documentación de Swagger
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-// Definir una ruta para la raíz
-app.get('/', (req, res) => {
-  res.send('Bienvenido a la API del taller!');
-});
-
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
